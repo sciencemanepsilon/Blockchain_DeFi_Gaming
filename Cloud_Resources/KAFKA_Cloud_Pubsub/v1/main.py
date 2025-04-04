157641ce1f2df728d3c073af8ed5b477dcd8e0a1
ILAN <scienceman.epsilon@gmail.com>
2025-04-04 17:56:30 +0000

    topic batch publishing

--- Cloud_Resources/KAFKA_Cloud_Pubsub/v1/main.py
+++ Cloud_Resources/KAFKA_Cloud_Pubsub/v1/main.py
@@ -3,13 +3,25 @@ from fastapi import FastAPI, Request
 from fastapi.responses import PlainTextResponse
 from json import dumps
 from os import environ
+from concurrent import futures
 from google.cloud import pubsub_v1
-# called by: Login, Register, ChangeNickname
+# '/' called by: Login, Register, ChangeNickname
 
 app = FastAPI()
 dummyDict = {"key":"value"}
 publisher = pubsub_v1.PublisherClient()
+maxmsg = int(environ['PUBSUB_BATCH_MSG_SIZE_TRESHOLD'])
+maxbytes = int(environ['PUBSUB_BATCH_BYTES_TRESHOLD'])
+maxtime = int(environ['PUBSUB_BATCH_SECONDS_TRESHOLD'])
 print("publisher client created")
+
+# Configure batch to publish when 1000 messages or 5 MB of data, or 3 second passed
+PublisherBatch = pubsub_v1.PublisherClient(pubsub_v1.types.BatchSettings(
+    max_messages=maxmsg,  # default 100
+    max_bytes=maxbytes,  # default 1 MB
+    max_latency=maxtime  # default 10 ms
+))
+print(f"publisher-batch created, msg {maxmsg} bytes {maxbytes} t {maxtime}")
 
 
 
@@ -35,6 +47,27 @@ async def makePublish(request: Request):
     return PlainTextResponse(content="success", status_code=200)
 
 
+# called by Sport Bet Api
+@app.post('/Batch', response_class=PlainTextResponse)
+async def makePublish(request: Request):
+    reqData = await request.json()
+    topicPath = reqData['topicPath']
+    jsonData = reqData['jsonData']
+    attributes = reqData.get('attributes')
+
+    if not topicPath:
+        print("Batch: missing topic path, End")
+        return PlainTextResponse(content="no data", status_code=283)
+    if not attributes: attributes = dummyDict
+    print(f"Batch: topicPath {topicPath} att {attributes} JsonKeys {jsonData.keys()}")
+
+    err = callPubsubBatch(topicPath, jsonData, attributes)
+    if err:
+        print(err, " Batch: End")
+        return PlainTextResponse(content=err, status_code=284)
+    return PlainTextResponse(content="success", status_code=200)
+
+
 
 def newTopicPath(topicPath):
     if "/" not in topicPath: return False, "invalid tpath"
@@ -51,6 +84,30 @@ def callPubsub(topicPath, msg, attributes):
     except Exception as e:
         print(f"pubsub call failed: {e}")
         return "pubsub call failed"
+    return False
+
+
+def callback(future: pubsub_v1.publisher.futures.Future) -> None:
+    try: print(f"Batch: pubsub-call msg-id ", future.result())
+    except Exception as e: print(f"Batch: future.result() failed: {e}")
+
+
+def callPubsubBatch(topicPath, jsDict, attributes):
+    PromiseArr, jsArr, jsFields = [], None, {}
+    for ky in jsDict.keys():
+        if not isinstance(jsDict[ky], list): jsFields[ky] = jsDict[ky]
+        else: jsArr = jsDict[ky]
+    print(f"Batch Arr {jsArr} att {jsFields}")
+    if not jsArr: return "empty array"
+    
+    for js in jsArr:
+        msg = dumps({"ArrayItem":js, **jsFields})
+        publish_future = PublisherBatch.publish(topicPath, msg.encode("utf-8"), **attributes)
+        publish_future.add_done_callback(callback)
+        PromiseArr.append(publish_future)
+
+    futures.wait(PromiseArr, timeout=8, return_when=futures.ALL_COMPLETED)
+    print(f"Batch: Published messages-batch {len(PromiseArr)}, End")
     return False
 
 if __name__ == "__main__":
